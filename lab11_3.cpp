a2432e5122e86e773c8688f08214f4da324a1150
bewzunjo <90750971+bewzunjo@users.noreply.github.com>
2021-12-17 17:37:52 +0700

    Update lab11_3.cpp

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,24 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int N = 0;
+    double mean, d, sum = 0, a = 0;
+    ifstream source;
+    source.open("score.txt");
+    string score;
+    while(getline(source,score)){
+        sum += atof(score.c_str());
+        a += pow(atof(score.c_str()),2);
+        N++; 
+    }
+    mean = sum/N;
+    d = sqrt((a/N)-pow(mean,2));
+    cout << "Number of data = "<< N << endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< mean << endl;
+    cout << "Standard deviation = "<< d;
+}
